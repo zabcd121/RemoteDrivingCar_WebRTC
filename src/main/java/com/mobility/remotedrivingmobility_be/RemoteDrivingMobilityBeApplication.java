1f7dbacdf263d6e17e465219f0356874e757d2bb
zabcd121 <zabcd121@naver.com>
2023-03-30 20:10:33 +0900

    Start: 프로젝트 생성

---
+++ src/main/java/com/mobility/remotedrivingmobility_be/RemoteDrivingMobilityBeApplication.java
@@ -0,0 +1,13 @@
+package com.mobility.remotedrivingmobility_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteDrivingMobilityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteDrivingMobilityBeApplication.class, args);
+    }
+
+}
