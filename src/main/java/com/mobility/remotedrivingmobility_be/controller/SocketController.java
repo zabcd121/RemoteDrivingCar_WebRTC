18f71def6276f7229b58f4af1c97f007b9f7a7d0
zabcd121 <zabcd121@naver.com>
2023-04-27 17:51:42 +0900

    Create SocketController.java

---
+++ src/main/java/com/mobility/remotedrivingmobility_be/controller/SocketController.java
@@ -0,0 +1,11 @@
+package com.mobility.remotedrivingmobility_be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SocketController {
+
+    //@GetMapping("/offer")
+
+}
